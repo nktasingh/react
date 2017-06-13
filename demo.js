761e119567299f273d1b48a9d7dddccbfc785e83
Nikita Singh <nktasingh@gmail.com>
2017-06-13 11:38:18 -0400

    Displays!

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('test')
-);
+// var element = <h1>Hello, world!</h1>;
+var element = React.createElement('h1', null, 'Hello, world!');
+var container = document.getElementById('test');
+ReactDOM.render(element, container);
